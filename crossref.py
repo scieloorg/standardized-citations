da6811ad784b9d0d71792dcf114ad8493876e336
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-28 22:00:01 -0300

    Adiciona classe CrossrefAsyncCollector com parâmetros básicos

--- crossref.py
+++ crossref.py
@@ -25,3 +25,19 @@ ENDPOINT_CROSSREF_OPENURL = 'https://doi.crossref.org/openurl?'
 SEMAPHORE_LIMIT = 20
 
 
+class CrossrefAsyncCollector(object):
+
+    def __init__(self, email: None, mongo_database, mongo_collection, mongo_host=None):
+        self.email = email
+
+        if mongo_host:
+            try:
+                self.persist_mode = 'mongo'
+                self.mongo = MongoClient(mongo_host)[mongo_database][mongo_collection]
+                total_docs = self.mongo.count_documents({})
+                logging.info('There are {0} documents in the mongo {1}.{2}'.format(total_docs, mongo_database, mongo_collection))
+            except errors.ConnectionFailure as e:
+                logging.error('ConnectionFailure %s' % e)
+        else:
+            self.persist_mode = 'json'
+            self.path_results = 'crossref-results-' + str(time.time()) + '.json'
