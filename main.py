64430c2050fd155b1ee8f8958b02c3b8f9344f12
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-28 21:58:19 -0300

    Adiciona métodos auxiliares para informação de padronização

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ from time import time
 DEFAULT_MONGO_DATABASE_NAME = 'citations'
 DEFAULT_MONGO_COLLECTION_NAME = 'standardized'
 
+
+def format_date(date: datetime):
+    if not date:
+        return None
+    return date.strftime('%Y-%m-%d')
+
+
+def get_execution_mode(use_exact, use_fuzzy):
+    info = []
+
+    if use_exact:
+        info.append('Exact is on')
+    else:
+        info.append('Exact is off')
+
+    if use_fuzzy:
+        info.append('Fuzzy is on')
+    else:
+        info.append('Fuzzy is off')
+
+    return ' - '.join(info)
+
+
