ff7bdcd8c2d77727f96882ae6b184eacba352df1
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-28 21:42:27 -0300

    Adiciona método para limpar ISSNs

--- utils/generate_db.py
+++ utils/generate_db.py
@@ -5,3 +5,16 @@ import pickle
 import textwrap
 
 from datetime import datetime
+
+
+def clean_issn(issn: str):
+    """
+    Verifica se ISSN está no formato padrao 'DDDD-DDDD' e remove hífen.
+
+    :param issn: ISSN
+    :return: ISSN limpo (sem o hífen)
+    """
+    if len(issn) == 9 and '-' in issn:
+        return ''.join([issn[:4], issn[5:]])
+    elif len(issn) == 8 and '-' not in issn:
+        return issn
