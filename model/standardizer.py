52d95c33685ec94d3c7b7b6e45ad81a0e84d3685
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-28 21:49:22 -0300

    Cria classe de padronização com atributos obrigatórios e não obrigatórios

--- model/standardizer.py
+++ model/standardizer.py
@@ -31,3 +31,35 @@ STATUS_FUZZY_VOLUME_INFERRED_VALIDATED_LR_ML1 = 13
 VOLUME_IS_ORIGINAL = 0
 VOLUME_IS_INFERRED = 1
 VOLUME_NOT_USED = -1
+
+
+class Standardizer:
+
+    logging.basicConfig(level=logging.INFO)
+
+    def __init__(self,
+                 path_db,
+                 mongo_database,
+                 mongo_collection,
+                 use_exact=False,
+                 use_fuzzy=False,
+                 mongo_host=None):
+
+        self.use_exact = use_exact
+        self.use_fuzzy = use_fuzzy
+
+        if mongo_host:
+            try:
+                self.persist_mode = 'mongo'
+                self.mongo = MongoClient(mongo_host)[mongo_database][mongo_collection]
+                total_docs = self.mongo.count_documents({})
+                logging.info('There are {0} documents in the mongo {1}.{2}'.format(total_docs, mongo_database, mongo_collection))
+            except errors.ConnectionFailure as e:
+                logging.error('ConnectionFailure %s' % e)
+        else:
+            self.persist_mode = 'json'
+            self.path_results = 'std-results-' + str(time.time()) + '.json'
+
+        if path_db:
+            logging.info('Loading %s' % path_db)
+            self.db = self.load_database(path_db)
