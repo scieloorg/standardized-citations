f940f1c37f7814f088c2b7736158826ab14cfc7c
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-28 21:39:37 -0300

    Adiciona método para remover caracteres inválidos

--- utils/string_processor.py
+++ utils/string_processor.py
@@ -6,3 +6,18 @@ import unicodedata
 parenthesis_pattern = re.compile(r'[-a-zA-ZÀ-ÖØ-öø-ÿ|0-9]*\([-a-zA-ZÀ-ÖØ-öø-ÿ|\W|0-9]*\)[-a-zA-ZÀ-ÖØ-öø-ÿ|0-9]*', re.UNICODE)
 special_chars = ['@', '&']
 special_words = ['IMPRESSO', 'ONLINE', 'CDROM', 'PRINT', 'ELECTRONIC']
+
+
+def remove_invalid_chars(text):
+    """
+    Remove de text os caracteres que possuem código ASCII < 32 e = 127.
+    :param text: texto a ser tratada
+    :return: texto com caracteres ASCII < 32 e = 127 removidos
+    """
+    vchars = []
+    for t in text:
+        if ord(t) == 11:
+            vchars.append(' ')
+        elif ord(t) >= 32 and ord(t) != 127:
+            vchars.append(t)
+    return ''.join(vchars)
