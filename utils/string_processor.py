4187df8624a8284d5ab9a8d251d9dcc88c9dc17c
Rafael J Pezzuto Damaceno <rafael.pezzuto@gmail.com>
2020-05-28 21:40:26 -0300

    Adiciona método para remover caracteres não alfanuméricos

--- utils/string_processor.py
+++ utils/string_processor.py
@@ -30,3 +30,20 @@ def remove_accents(text):
     :return: texto sem caracteres acentuados
     """
     return unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore').decode('ASCII')
+
+
+def alpha_num_space(text, include_special_chars=False):
+    """
+    Mantém em text apenas caracteres alpha, numéricos e espaços.
+    Possibilita manter em text caracteres especiais na lista special_chars
+    :param text: texto a ser tratado
+    :param include_special_chars: booleano que indica se os caracteres especiais devem ou não ser mantidos
+    :return: texto com apenas caracteres alpha e espaço mantidos (e especiais, caso solicitado)
+    """
+    new_str = []
+    for character in text:
+        if character.isalnum() or character.isspace() or (include_special_chars and character in special_chars):
+            new_str.append(character)
+        else:
+            new_str.append(' ')
+    return ''.join(new_str)
