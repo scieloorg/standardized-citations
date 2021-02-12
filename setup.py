9b88ce329d618ff498c4df339afb5274132656b4
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-02-12 16:59:38 -0300

    Adiciona setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'aiohttp==3.7.3',
+    'articlemetaapi==1.26.7',
+    'asyncio==3.4.3',
+    'lxml==4.6.2',
+    'pymongo==3.11.3',
+    'xmltodict==0.12.0',
+    'xylose==1.35.4',
+]
+
+setup(
+    name="standardized-citations",
+    version='0.1',
+    description="The SciELO Standardization Tools",
+    author="SciELO",
+    author_email="scielo-dev@googlegroups.com",
+    license="BSD",
+    url="https://github.com/scieloorg/standardized-citations",
+    keywords='cited references, normalization, deduplication',
+    maintainer_email='rafael.pezzuto@gmail.com',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points="""
+    [console_scripts]
+    normalize=proc.normalize:main
+    crossref=proc.crossref:main
+    """
+)
